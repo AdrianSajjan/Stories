b812fa584b07577a97598d9c2e58fb0101fadbf8
AdrianSajjan <50446145+AdrianSajjan@users.noreply.github.com>
2020-04-20 16:56:41 +0530

    Added Sticky Sidebar

--- client/src/components/Discover/Discover.jsx
+++ client/src/components/Discover/Discover.jsx
@@ -5,40 +5,28 @@ import { connect } from "react-redux";
 import ProfileCard from "../Profile/Profile-Card/Profile-Card";
 
 const Discover = () => {
-  const sideAreaRef = useRef(null);
-  const [sticky, setSticky] = useState(false);
+  const rootRef = useRef(null);
+  const [top, setTop] = useState(0);
 
   useEffect(() => {
-    window.addEventListener("scroll", ToggleSticky);
+    if (!rootRef || !rootRef.current) return;
 
-    return () => {
-      window.removeEventListener("scroll", ToggleSticky);
-    };
-  }, []);
+    const clientHeight = rootRef.current.clientHeight;
+    const relativeMargin = clientHeight - window.innerHeight;
 
-  const ToggleSticky = (event) => {
-    if (!sideAreaRef || !sideAreaRef.current) return;
-
-    console.log(`client : ${sideAreaRef.current.clientHeight}`);
-    console.log(`window : ${window.innerHeight}`);
-  };
+    if (relativeMargin > 0) setTop(relativeMargin + 10);
+  }, [rootRef]);
 
   return (
     <Fragment>
-      <div
-        ref={sideAreaRef}
-        className={`side-area-container ${sticky && "sticky"}`}
-      >
+      <div ref={rootRef} className="side-area-container" style={{ top: -top }}>
         <div className="side-area-card">
           <p className="side-area-card-title text-center text-secondary">
             Discover Profiles
           </p>
           <hr />
           <ProfileCard />
-          <ProfileCard />
-          <ProfileCard />
-          <ProfileCard />
-          <ProfileCard />
+
           <Button color="info" className="mx-auto d-block mb-1">
             Load More
           </Button>
