61a6b7518b9df521a8a288bd1cbdfc2f501e4dc9
AdrianSajjan <adriansajjan2001@gmail.com>
2020-05-08 14:47:02 +0530

    Made some production changes

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,72 +1,71 @@
-const express = require("express");
-const Profile = require("../../models/Profile");
-const auth = require("../../middleware/token-auth");
-const multer = require("multer");
-const cloudinary = require("cloudinary");
-const cloudinaryStorage = require("multer-storage-cloudinary");
+const express = require('express')
+const Profile = require('../../models/Profile')
+const auth = require('../../middleware/token-auth')
+const multer = require('multer')
+const cloudinary = require('cloudinary')
+const cloudinaryStorage = require('multer-storage-cloudinary')
 
-require("dotenv").config();
-const router = express.Router();
+const { SERVER } = require('../../config/errors')
+
+require('dotenv').config()
+const router = express.Router()
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: "uploads",
-  allowedFormats: ["jpg", "png", "jpeg"],
-});
+  folder: 'uploads',
+  allowedFormats: ['jpg', 'png', 'jpeg']
+})
 
-const parser = multer({ storage: storage });
+const parser = multer({ storage: storage })
 
 /**
  * @route : POST api/upload/profile
  */
-router.post("/profile", [auth, parser.single("profile")], async (req, res) => {
+router.post('/profile', [auth, parser.single('profile')], async (req, res) => {
   try {
     const profile = await Profile.findOne({
-      user: req.user.id,
+      user: req.user.id
     })
-      .populate("user", ["name", "email"])
-      .populate("following.profile")
-      .populate("followers.profile");
+      .populate('user', ['name', 'email'])
+      .populate('following.profile')
+      .populate('followers.profile')
 
     if (!profile)
       return res.status(404).json({
         type: NOTFOUND,
-        msg: "Profile doesn't exist",
-      });
+        msg: "Profile doesn't exist"
+      })
 
     let avatar = {
       public_id: req.file.public_id,
-      url: req.file.url,
-    };
+      url: req.file.url
+    }
 
     if (profile.avatar && profile.avatar.public_id.length) {
-      cloudinary.v2.uploader.destroy(
-        profile.avatar.public_id,
-        async (error, result) => {
-          profile.avatar = avatar;
-          await profile.save();
+      cloudinary.v2.uploader.destroy(profile.avatar.public_id, async (error, result) => {
+        profile.avatar = avatar
+        await profile.save()
 
-          console.log(`Result: ${result} | Error: ${error}`);
-          return res.json(profile);
-        }
-      );
+        console.log(`Result: ${result} | Error: ${error}`)
+        return res.json(profile)
+      })
     } else {
-      profile.avatar = avatar;
+      profile.avatar = avatar
 
-      await profile.save();
+      await profile.save()
 
-      return res.json(profile);
+      return res.json(profile)
     }
   } catch (err) {
-    console.log(err.message);
-    res.status(500).send(SERVER);
+    console.log(err.message)
+    res.status(500).send(SERVER)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
